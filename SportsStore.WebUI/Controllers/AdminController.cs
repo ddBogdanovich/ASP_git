aa97cd6e48bb801fac231780f139677c0e3492cb
ddBogdanovich <dd.Bogdanovich@gmail.com>
2016-09-23 18:15:00 +0300

    Added AdminController

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+using System.Linq;
+using SportsStore.Domain.Entities;
+
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            return View(product);
+        }
+    }
+}
